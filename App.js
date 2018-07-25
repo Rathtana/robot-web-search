e47172535824ce585cdb515d1ac7d3763290b8d8
Rathtana Duong <drathtana@yahoo.com>
2018-07-25 01:11:57 -0700

    adding source code files

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import SearchBox from './SearchBox'
+import CardList from './CardList'
+import {robots} from './robots'
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {	//state is something that can change dynamically 
+			robots: robots,
+			searchfield: ''		
+		}
+	}
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+			<div className='tc'>
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList robots={filteredRobots}/>
+			</div>
+		);		
+	}	
+}
+
+export default App;
